2db27de66792f8c2f4e982dc78ee1b2d34bef09c
Daniel Thwaites <danthwaites30@btinternet.com>
2022-12-21 16:07:53 +0000

    Add a desktop entry :bento:

--- flake.nix
+++ flake.nix
@@ -13,9 +13,18 @@
         packages.default = pkgs.python3Packages.buildPythonApplication {
           name = "scribe";
           src = ./.;
+
           nativeBuildInputs = with pkgs; [ gobject-introspection wrapGAppsHook ];
           buildInputs = with pkgs; [ gtk4 libadwaita ];
           propagatedBuildInputs = with pkgs.python3Packages; [ pygobject3 torch transformers ];
+
+          postInstall = ''
+            mkdir -p $out/share/applications
+            cp ${./me.danth.Scribe.desktop} $out/share/applications/me.danth.Scribe.desktop
+            substituteInPlace $out/share/applications/me.danth.Scribe.desktop \
+              --replace /usr/bin/scribe $out/bin/scribe
+          '';
+
           doCheck = false;
         };
       }
