b3fe3c4d23231554fb0f95d4faa7c420898cd6f7
Daniel Thwaites <danthwaites30@btinternet.com>
2022-12-21 16:28:04 +0000

    Download model files through Nix :hammer:
    This means that the model is downloaded during installation rather
    than when it is first used.
    
    If Nix is not being used then the model will be downloaded on first
    use as it was previously.

--- flake.nix
+++ flake.nix
@@ -8,11 +8,24 @@
     { nixpkgs, utils, ... }:
     utils.lib.eachDefaultSystem (
       system:
-      let pkgs = nixpkgs.legacyPackages.${system};
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        model = pkgs.fetchgit {
+          url = "https://huggingface.co/facebook/opt-1.3b";
+          rev = "aa6ac1e23bb9a499be2b7400079cd2a7b8a1309a";
+          fetchLFS = true;
+          sha256 = "";
+        };
       in {
         packages.default = pkgs.python3Packages.buildPythonApplication {
           name = "scribe";
           src = ./.;
+
+          postPatch = ''
+            substituteInPlace $src/scribe/__main__.py \
+              --replace facebook/opt-1.3b ${model}
+          '';
 
           nativeBuildInputs = with pkgs; [ gobject-introspection wrapGAppsHook ];
           buildInputs = with pkgs; [ gtk4 libadwaita ];
